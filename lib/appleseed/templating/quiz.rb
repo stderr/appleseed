bd760360a0987e589bbb1240328459231d52ae74
Spencer Olson <solson@instructure.com>
2014-10-15 11:32:41 -0500

    wip - add seeding functionality for Quizzes

--- lib/appleseed/templating/quiz.rb
+++ lib/appleseed/templating/quiz.rb
@@ -2,14 +2,29 @@ module Appleseed
   module Templating
     class Quiz < BaseTemplate
       #placeholder for to-be-determined data
-      attr_accessor :some_data
+      attr_accessor :title, :published, :course_id
 
       def initialize(data={})
-        @some_data = data[:some_data] || "some_default"
+        @published = data.fetch(:published, true)
+        @course_id = data.fetch(:course_id, 1)
       end
 
       def seed_data
+        {
+          quiz: {
+            title: title,
+            published: published
+          }
+        }
+      end
 
+      def title
+        @title ||= [Faker::Hacker.ingverb,
+                    Faker::Hacker.noun.pluralize].join(" ").titleize
+      end
+
+      def scoped_attrs
+        [course_id]
       end
     end
   end

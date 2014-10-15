9d89e7f8283885f97dab4ecdb3cd852349c039de
Spencer Olson <solson@instructure.com>
2014-10-15 15:59:21 -0500

    add/remove specs from quiz_spec

--- spec/templating/quiz_spec.rb
+++ spec/templating/quiz_spec.rb
@@ -1,43 +1,59 @@
 require 'spec_helper'
 
 module Appleseed
-  module Templating
-    describe Quiz do
-    	let(:quiz) { Quiz.new }
+	module Templating
+		describe Quiz do
+			let(:quiz) { Quiz.new }
 
-    	context "#initialize" do
-    		it "defaults to published" do
-        	expect(quiz.published).to eq(true)
-      	end
+			context "#initialize" do
+				it "defaults to published" do
+					expect(quiz.published).to eq(true)
+				end
 
-      	it "defaults to an assignment type" do
-   				expect(quiz.quiz_type).to eq("assignment")
-      	end
+				it "defaults to an assignment type" do
+					expect(quiz.quiz_type).to eq("assignment")
+				end
 
-      	it "defaults to a course_id of 1" do
-      		expect(quiz.course_id).to eq(1)
-      	end
-    	end
+				it "defaults to a course_id of 1" do
+					expect(quiz.course_id).to eq(1)
+				end
+			end
 
-    	context "#scoped_attrs" do
-    		it "contains the course_id" do
-    			q = Quiz.new(course_id: 5)
-    			expect(q.scoped_attrs.size).to eq(1)
-    			expect(q.scoped_attrs).to include(5)
-    		end
-    	end
+			context "#seed_data" do
+				it "scopes to 'quiz'" do
+					expect(quiz.seed_data).to have_key(:quiz)
+				end
 
-  		context "#title" do
-  			it "should generate a non-blank title" do
-  				expect(quiz.title).not_to be_empty
-  			end
-  		end
+				context "attributes" do
+					it "has a title" do
+						expect(quiz.seed_data[:quiz]).to have_key(:title)
+						expect(quiz.seed_data[:quiz][:title]).not_to be_blank
+					end
 
-  		context "#description" do
-  			it "should generate a non-blank description" do
-  				expect(quiz.description).not_to be_empty
-  			end
-  		end
-    end
-  end
+					it "has a description" do
+						expect(quiz.seed_data[:quiz]).to have_key(:description)
+						expect(quiz.seed_data[:quiz][:description]).not_to be_blank
+					end
+
+					it "has a quiz_type" do
+						expect(quiz.seed_data[:quiz]).to have_key(:quiz_type)
+						expect(quiz.seed_data[:quiz][:quiz_type]).not_to be_blank
+					end
+
+					it "has a published attribute" do
+						expect(quiz.seed_data[:quiz]).to have_key(:published)
+						expect(quiz.seed_data[:quiz][:published]).not_to be_blank
+					end
+				end
+			end
+
+			context "#scoped_attrs" do
+				it "contains the course_id" do
+					q = Quiz.new(course_id: 5)
+					expect(q.scoped_attrs.size).to eq(1)
+					expect(q.scoped_attrs).to include(5)
+				end
+			end
+		end
+	end
 end

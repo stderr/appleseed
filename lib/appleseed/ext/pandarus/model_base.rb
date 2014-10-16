a2011a0033822b0ef2165d78396628845bacd897
Josh Simpson <jsimpson@instructure.com>
2014-10-15 22:20:46 -0600

    use alias_method for overriding pandarus

--- lib/appleseed/ext/pandarus/model_base.rb
+++ lib/appleseed/ext/pandarus/model_base.rb
@@ -1,12 +1,11 @@
 module Pandarus
   class ModelBase
-    remove_method :initialize
+    # Unfortunately, Pandarus has issues with the newish JSONAPI
+    # style of some API endpoints in Canvas. This will fix it.
+    alias_method :original_initialize, :initialize
     def initialize(attributes = {})
       attributes = attributes.first if attributes.is_a?(Array)
-      return if attributes.empty?
-      attributes.each_pair do |name, value|
-        assign(name, value)
-      end
+      original_initialize(attributes)
     end
   end
 end
